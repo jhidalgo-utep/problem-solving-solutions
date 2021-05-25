e65806d15a1377906e12e2c2b1e5b3195328ca9f
jhidalgo-utep <jhidalgo.utep@gmail.com>
2021-05-25 10:11:03 -0600

    I am currently working on this file, do not touch for now please as im in the running test of branches. just adding more to this file

--- data-structures/LinkedList/main.py
+++ data-structures/LinkedList/main.py
@@ -5,4 +5,6 @@ Created on Tue May 25 09:43:21 2021
 @author: joaquin
 """
 
-print("HELLO WORLD !!!")
+# print("HELLO WORLD !!!")
+print("I just need to make some changes here...")
+print("... Should only take 1 sec.")
