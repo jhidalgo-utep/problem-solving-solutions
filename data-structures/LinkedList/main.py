24f197f3a2fee7b3c043372a7e15a0ab931e7730
jhidalgo-utep <jhidalgo.utep@gmail.com>
2021-05-26 11:23:13 -0600

    added just the Node Class which is part of the data structure

--- data-structures/LinkedList/main.py
+++ data-structures/LinkedList/main.py
@@ -5,6 +5,17 @@ Created on Tue May 25 09:43:21 2021
 @author: joaquin
 """
 
-# print("HELLO WORLD !!!")
-print("I just need to make some changes here...")
-print("... Should only take 1 sec.")
+class Node(object):
+    def _init__(self, data = None, next = None):
+        self.data = data
+        self.next = next
+        
+    def get_next(self):
+        return self.next
+    
+    def get_data(self):
+        return self.data
+    
+    def set_data(self, newItem):
+        self.data = newItem
+        
