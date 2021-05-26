f53dfa1b49f335c8f784bc418dbe9c8a73756370
jhidalgo-utep <jhidalgo.utep@gmail.com>
2021-05-26 12:13:55 -0600

    Added insert, isEmpty and printLL to ensure Data Struct is working correctly. It is in a running state

--- data-structures/LinkedList/main.py
+++ data-structures/LinkedList/main.py
@@ -6,7 +6,7 @@ Created on Tue May 25 09:43:21 2021
 """
 
 class Node(object):
-    def _init__(self, data = None, next = None):
+    def __init__(self, data = None, next = None):
         self.data = data
         self.next = next
         
@@ -19,8 +19,29 @@ class Node(object):
     def set_data(self, newItem):
         self.data = newItem
         
+    def set_next(self, nextNode):
+        self.next = nextNode
+    
+        
 class LinkedList(object):
     def __init__(self, head = None, tail = None):
         self.head = head
         self.tail = tail
         
+def isEmpty(L):
+    return L.head == None
+    
+def insert(L, newItem):
+    if isEmpty(L):
+        L.head = Node(newItem)
+        L.tail = L.head
+    else:
+        Node.set_next(L.tail, Node(newItem))
+        L.tail = L.tail.next
+        
+def printLL(L):
+    iter = L.head
+    while iter != None:
+        print(iter.data)
+        iter = Node.get_next(iter)
+
