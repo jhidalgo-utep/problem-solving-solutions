0ef64d2787aeab0f27707dea0916c4e17d708fd0
jhidalgo-utep <jhidalgo.utep@gmail.com>
2021-05-27 22:14:14 -0600

    added comments to Node.py file

--- data-structures/LinkedList/Node.py
+++ data-structures/LinkedList/Node.py
@@ -8,18 +8,24 @@ Created on Tue May 27 21:32:34 2021
 #Node Class
 class Node(object):
     
+    #Constructor takes in 2 optional parameters 'data' and 'next'. default parameters are 'None'
     def __init__(self, data = None, next = None):
-        self.data = data
-        self.next = next
+        self.data = data # 'data' attribute holds integers
+        self.next = next # 'next' attribute holds the pointer to next node
         
+    #Method takes in a node as a parameter and returns the next node
     def get_next(self):
         return self.next
     
+    #Method takes in a node as a parameter and returns the data
     def get_data(self):
         return self.data
     
+    #Method takes in a node and integer to update the 'data' and returns nothing
     def set_data(self, newItem):
         self.data = newItem
         
+    #Method takes in 2 nodes one for self and another to set the next node and returns nothing
     def set_next(self, nextNode):
         self.next = nextNode
+        
