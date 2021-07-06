8723a521d0b9d0753700a6c2cc2c63fa252622e5
jhidalgo-utep <jhidalgo.utep@gmail.com>
2021-07-06 12:15:52 -0600

    practiced LinkedList implementation

--- data-structures/LinkedList/practice/runner_3.py
+++ data-structures/LinkedList/practice/runner_3.py
@@ -1,45 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  6 12:01:38 2021
+
+@author: joaqu
+"""
+
 class Node(object):
-    def __init__(self, data = None, next = None):
-        self.data = data
+    def __init__(self, item = None, next = None):
+        self.item = item
         self.next = next
     
-    #Self parameter is the Node object/class
+    #The self parameter refers to the self class 'Node'
     def get_next_node(self):
         return self.next
     
-    def get_node_data(self):
-        return self.data
+    def get_item(self):
+        return self.item
 
 class LinkedList(object):
     def __init__(self):
         self.head = None
         self.tail = None
     
-    #Self parameter is the linkedlist obj
-    def insert(self, insert_data):
+    def insert(self, item):
         if self.head == None:
-            self.head = Node(insert_data)
+            self.head = Node(item)
             self.tail = self.head
         else:
-            self.tail.next = Node(insert_data)
+            self.tail.next = Node(item)
             self.tail = self.tail.next
     
-    def print_list(self):
+    def display(self):
         if self.head == None:
             return
         iter_node = self.head
         while iter_node != None:
-            print(iter_node.get_node_data() )
-            iter_node = iter_node.get_next_node()
-    
-    
-        
+            print(iter_node.get_item() )
+            iter_node = iter_node.next
 
 if __name__ == "__main__":
-    my_list = LinkedList()
-    my_list.insert(33)
-    my_list.insert(34)
-    my_list.insert(35)
-    my_list.insert(36)
-    
-    my_list.print_list()
+    ll = LinkedList()
+    ll.insert(21)
+    ll.insert(24)
+    ll.insert(93)
+    ll.display()
