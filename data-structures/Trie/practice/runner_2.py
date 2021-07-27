f3d95ac0e8630ce3660de04b36bbcd002afcf87d
jhidalgo-utep <jhidalgo.utep@gmail.com>
2021-07-26 22:15:50 -0600

    added custom data struct

--- data-structures/Trie/practice/runner_2.py
+++ data-structures/Trie/practice/runner_2.py
@@ -4,8 +4,59 @@ Created on Mon Jul 26 19:26:57 2021
 
 @author: joaqu
 """
+class Node(object):
+    def __init__(self, letter = None):
+        self.letter = letter
+        self.children = {}
+        self.is_leaf = False
+        
+
+class Trie(object):
+    def __init__(self):
+        self.root = Node('*')  
+        
+        
+    def insert(self, word):
+        iter_node = self.root
+        for c in word:
+            if c not in iter_node.children:
+                iter_node.children[c] = Node(c)
+            iter_node = iter_node.children[c]
+        iter_node.is_leaf = True
+        
+        
+    def search(self, word):
+        iter_node = self.root
+        for c in word:
+            if c not in iter_node.children:
+                return False
+            iter_node = iter_node.children[c]
+            
+        if iter_node.is_leaf:
+            return True
+        return False
+    
+    
+    def is_prefix(self, word):
+        iter_node = self.root
+        for c in word:
+            if c not in iter_node.children:
+                return False
+            iter_node = iter_node.children[c]
+            
+        return True
 
 
 
 if __name__ == "__main__":
-    print('start program')
+    print('start program\n')
+    
+    tr = Trie()
+    tr.insert("playing")
+    tr.insert("play")
+    tr.insert("played")
+    print( tr.is_prefix("*") )
+    
+    
+    
+    
