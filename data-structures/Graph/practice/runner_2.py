cf2703a16de851a5a554fee5765265e5bd69255f
jhidalgo-utep <jhidalgo.utep@gmail.com>
2021-07-30 14:09:52 -0600

    added topolgical sort and kruskal min span tree

--- data-structures/Graph/practice/runner_2.py
+++ data-structures/Graph/practice/runner_2.py
@@ -7,6 +7,42 @@ Created on Sun Jul 25 11:35:35 2021
 import numpy as np
 
 
+class DisjointForrestSet(object):
+    def __init__(self, size):
+        self.item = np.zeros(size, dtype=np.int) - 1
+    
+    def display(self):
+        print(self.item)
+        
+     #Find with path
+    def find_root(self, index):
+        if self.item[index] < 0:
+            return index
+        return self.find_root( self.item[index] )
+
+    #Union by Size
+    def union(self, i, j):
+        ri = self.find_root(i)
+        rj = self.find_root(j)
+        #checks if in same subset
+        if ri != rj:
+            #Checks which dsf is smalller
+            
+            if self.item[ri] > self.item[rj]:
+                self.item[rj] += self.item[ri]  #sum up both and store in least [rj]
+                self.item[ri] = rj #tell the greater one [ri] where the least  [ri] one is
+            else:
+                self.item[ri] += self.item[rj]
+                self.item[rj] = ri            
+                
+    def check_same_set(self, i, j):
+        root1 = self.find_root(i)
+        root2 = self.find_root(j)
+        if root1 != root2:
+            return False
+        return True
+
+#####################################################
 class AdjacencyList(object):
     def __init__(self, nodes):
         self.node = nodes
@@ -14,6 +50,17 @@ class AdjacencyList(object):
         self.item = []
         for i in range(self.num_of_nodes):
             self.item.append([])
+    
+    def in_degree(self):
+        result = []
+        for vertice in range(len(self.item) ):
+            result.append(0)
+        
+        for i in range(len(self.item) ):
+            for j in range(len(self.item[i]) ):
+                result[ self.item[i][j] ] += 1
+        return result
+        
             
     def display(self):
         print(self.item)
@@ -78,8 +125,31 @@ class AdjacencyList(object):
                 if neighbour not in visit_complete:
                     visit_complete.append(neighbour)
                     stack.append(neighbour)
-                    #Here you can: prev[neighbour] = current : to state where you came from                                        
-                    
+                    #Here you can: prev[neighbour] = current : to state where you came from
+
+    def topological(self):
+        result = []
+        queue = []
+        indegree = self.in_degree()
+        for i in range(len(indegree)):
+            if indegree[i] == 0:
+                queue.append(i)
+        
+        
+        while len(queue) > 0:
+            vertex = queue.pop(0)
+            result.append(vertex)
+            for u in self.item[vertex]:
+                indegree[u] = indegree[u] - 1
+                if indegree[u] == 0:
+                    queue.append(u)
+        
+        
+        if len(result) == len(self.item):
+            return result
+        else:
+            return []
+        
                     
 ##################################
 ##################################
@@ -99,6 +169,13 @@ class EdgeList(object):
             index2 = item2
             self.item.append([index1,index2])
             
+    def insert_list(self, new_list):
+        for i in new_list:
+            self.item.append([i[0], i[1], i[2]])
+        
+        
+            
+            
     def display(self):
         print(self.item)
         
@@ -117,6 +194,19 @@ class EdgeList(object):
             m[self.item[i][0]][self.item[i][1]] = True
             # m[self.item[i][1]][self.item[i][0]] = True  # For undirected graph
         return m
+    
+    
+    # edge_list = self.item[edge1,edge1,weight]
+    def kruskal(self):
+        self.item = sorted(self.item, key=lambda a: a[2])
+        ds = DisjointForrestSet(len(self.item))
+        min_span_tree = []
+        for edge in self.item:
+            if ds.check_same_set(edge[0], edge[1]) == False:
+                min_span_tree.append(edge)
+                ds.union(edge[0], edge[1])
+        return min_span_tree
+        
     
 
 ##################################
@@ -163,22 +253,32 @@ class AdjacencyMatrix(object):
 
 if __name__ == "__main__":
     print('start program\n')
-    n = ["Jack", "Nano", "Kitty", "Food", "Water", "Grass", "Treat", "Bread", "Healthy", "Obesse"]
-    
+
+    n = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"]
     a = AdjacencyList(n)
-    a.insert("Jack", "Nano")
-    a.insert("Jack", "Kitty")
-    a.insert("Nano", "Food")
-    a.insert("Nano", "Grass")
-    a.insert("Nano", "Treat")
-    a.insert("Kitty", "Water")
-    a.insert("Food", "Obesse")
-    a.insert("Treat", "Bread")
-    a.insert("Water", "Healthy")
-    a.display_nodes()
-    a.display()
-    print()
-    print()
-    a.dfs(0, [] )
+    
+    a.insert("I", "L")
+    a.insert("F", "K")
+    a.insert("E", "D")
+    a.insert("C", "A")
+    a.insert("H", "J")
+    a.insert("D", "G")
+    a.insert("E", "A")
+    a.insert("F", "J")
+    a.insert("D", "H")
+    a.insert("J", "L")
+    a.insert("A", "D")
+    a.insert("E", "F")
+    a.insert("H", "I")
+    a.insert("K", "J")
+    a.insert("G", "I")
+    a.insert("B", "D")
+    a.insert("J", "M")
+    a.insert("C", "B")
+    res = a.topological()
+    print(res)
+    
+    
+    
     
     
