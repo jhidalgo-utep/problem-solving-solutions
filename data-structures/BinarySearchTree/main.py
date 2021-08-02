f9ad1146f1a92b4f69d91dd322ffd8fd0dfd42a6
jhidalgo-utep <jhidalgo.utep@gmail.com>
2021-08-02 12:39:27 -0600

    prepared last data structure to work correctly

--- data-structures/BinarySearchTree/main.py
+++ data-structures/BinarySearchTree/main.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul  6 12:51:50 2021
+Created on Tue Jul  6 15:34:21 2021
 
 @author: joaqu
 """
-print('hi')
+from BST import BST
+
+if __name__ == "__main__":
+    print('start program')
+    b = BST()
+    b.insert(10)
+    b.insert(2)
+    b.insert(18)
+    b.display('')
