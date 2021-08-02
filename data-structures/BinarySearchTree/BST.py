9cbf4223c10c61e538151b052bbc3760c477379d
jhidalgo-utep <jhidalgo.utep@gmail.com>
2021-08-02 15:48:56 -0600

    added all corect methods

--- data-structures/BinarySearchTree/BST.py
+++ data-structures/BinarySearchTree/BST.py
@@ -4,5 +4,131 @@ Created on Fri Jul 16 12:13:44 2021
 @author: joaqu
 """
 
-
+class BST(object):
+    def __init__(self, new_item = None):
+        self.item = new_item
+        self.left = None
+        self.right = None
+        
+    def get_item(self):
+        return self.item
+        
+    def insert(self, new_item):
+        if self.item == None:
+            self.item = new_item
+        else:
+            if new_item < self.item:
+                if self.left == None:
+                    self.left = BST(new_item)
+                else:
+                    BST.insert(self.left, new_item)
+            else:
+                if self.right == None:
+                    self.right = BST(new_item)
+                else:
+                    BST.insert(self.right, new_item)
     
+    def display(self, space):
+        if self == None or self.item == None:
+            return
+        BST.display(self.right, space + '   ')
+        print(space, self.get_item() )
+        BST.display(self.left, space + '   ')
+        
+    def sum_at_depth(self, depth):
+        if self == None:
+            return 0
+        if depth == 0:
+            return self.get_item()
+        return BST.sum_at_depth(self.left, depth-1) + BST.sum_at_depth(self.right, depth-1)
+    
+    def find_depth_of_item(self, key_item):
+        if self == None or self.item == None:
+            return -1
+        if self.get_item() == key_item:
+            return 0
+        if key_item < self.get_item():
+            d =  BST.find_depth_of_item(self.left, key_item)
+        else:
+            d = BST.find_depth_of_item(self.right, key_item) 
+        
+        if d < 0:
+            return -1
+        else:
+            return d+1    
+    
+    def smallest(self):
+        if self == None or self.item == None:
+            return
+        if self.left == None:
+            return self.get_item()
+        return BST.smallest(self.left)
+    
+    def largest(self):
+        if self == None or self.item == None:
+            return
+        if self.right == None:
+            return self.get_item()
+        return BST.largest(self.right)
+    
+    def find(self, key_item):
+        if self == None or self.get_item() == None:
+            return False
+        if self.get_item() == key_item:
+            return True
+        if key_item < self.get_item():
+            return BST.find(self.left, key_item)
+        return BST.find(self.right, key_item)
+    
+    def print_at_depth(self, depth):
+        if self == None or self.item == None:
+            return
+        elif depth == 0:
+            print(self.get_item(), end = ' ' )
+        BST.print_at_depth(self.left, depth-1)
+        BST.print_at_depth(self.right, depth-1)   
+        
+    def sum_of_nodes(self):
+        if self == None or self.get_item() == None:
+            return 0
+        return self.get_item() + BST.sum_of_nodes(self.left) + BST.sum_of_nodes(self.right)
+    
+    def num_of_nodes(self):
+        if self == None or self.get_item() == None:
+            return 0
+        return 1 + BST.num_of_nodes(self.left) + BST.num_of_nodes(self.right)
+    
+    def extract_to_list(self, result):
+        if self == None:
+            return
+        BST.extract_to_list(self.left, result)
+        result.append(self.get_item() )
+        BST.extract_to_list(self.right, result)
+        
+        
+    ### PRACTICE!! ###
+    def build_BST(self, list_of_item):
+        if list_of_item == None or len(list_of_item) == 0:
+            return
+        mid_index = len(list_of_item) // 2
+        self = BST(list_of_item[mid_index] )
+        self.left = BST.build_BST(self.left, list_of_item[:mid_index])
+        self.right = BST.build_BST(self.right, list_of_item[mid_index+1:])
+        return self        
+    
+    ### PRACTICE!! ###
+    def height(self):
+        if self == None or self.get_item() == None:
+            return 0
+        
+        left_sub_tree_height = BST.height(self.left)
+        right_sub_tree_height = BST.height(self.right)
+        
+        if self.left == None and self.right == None:
+            return max(left_sub_tree_height, right_sub_tree_height)
+        
+        if left_sub_tree_height > right_sub_tree_height:
+            return left_sub_tree_height + 1
+        return right_sub_tree_height + 1
+        
+        
--- data-structures/BinarySearchTree/main.py
+++ data-structures/BinarySearchTree/main.py
@@ -9,7 +9,35 @@ from BST import BST
 if __name__ == "__main__":
     print('start program')
     b = BST()
-    b.insert(10)
-    b.insert(2)
-    b.insert(18)
+    b.insert(44)
+    b.insert(57)
+    b.insert(20)
+    b.insert(25)
+    b.insert(49)
+    b.insert(21)
+    b.insert(61)
+    b.insert(100)
     b.display('')
+    print('\n')
+    
+    print( b.sum_at_depth(6) )
+    print( b.find_depth_of_item(58) )
+    print(b.height() )
+    print(b.smallest() )
+    print(b.largest() )
+    print(b.find(100) )
+    print('\n')
+    print(b.print_at_depth(4
+                           ) )
+    print( b.sum_of_nodes() )
+    print( b.num_of_nodes() )
+    
+    bst_list = []
+    b.extract_to_list(bst_list)
+    print(bst_list)
+    print('\n')
+    
+    ##############
+    ages = [9,14,17,21, 22, 37, 40, 50]
+    b2 = BST.build_BST(None, ages)
+    b2.display('')
