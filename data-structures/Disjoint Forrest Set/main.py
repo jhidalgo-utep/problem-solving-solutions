3788aa3c19476d2007e7f903a1dfea4a02efd71d
jhidalgo-utep <jhidalgo.utep@gmail.com>
2021-08-02 16:23:50 -0600

    added full methods and data struct of disjoint forrest set

--- data-structures/Disjoint Forrest Set/main.py
+++ data-structures/Disjoint Forrest Set/main.py
@@ -4,4 +4,14 @@ Created on Wed Jul 21 20:31:36 2021
 
 @author: joaqu
 """
+from DisjointForrestSet import DSF
 
+if __name__ == "__main__":
+    print('start program\n')
+    d = DSF(10)
+    d.display()
+    d.union_by_size(0, 5)
+    d.display()
+    d.union_by_size(3, 8)
+    d.display()
+    print(d.num_of_sets() )
