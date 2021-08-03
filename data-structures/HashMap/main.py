d01eef52c71be4c30442fb038ef30aaf35afa541
jhidalgo-utep <jhidalgo.utep@gmail.com>
2021-08-02 22:58:50 -0600

    added custom and python defined hashmap

--- data-structures/HashMap/main.py
+++ data-structures/HashMap/main.py
@@ -1,2 +1,63 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  2 19:00:42 2021
 
+@author: joaqu
+"""
+from HashMap import HashMap
+
+if __name__ == "__main__":
+    print('start program\n')
+    
+    # ### Custom HashMap.py
+    # h = HashMap(2)
+    # h.insert('soccer')
+    # h.insert('basketball')
+    # h.insert('football')
+    # h.insert('pizza')
+    # h.display()
+    
+    
+    ### method 1
+    hm = dict()
+    
+    #insert
+    hm.update({'soccer':3})
+    hm['basketball'] = 7
+    hm['kitty'] = 10
+    hm['hockey'] = 2
+    hm.update({'polo': 8})
+    
+    #remove
+    hm.pop('soccer')
+    del hm['hockey']
+    
+    #increase value
+    hm['kitty'] = hm.get('kitty') + 1
+    hm['kitty'] += 1
+    
+    # for k in hm.keys():
+    #     print(k)
+        
+    # for v in hm.values():
+    #     print(v)
+        
+    # for k,v in hm.items():
+    #     print(v)
+    
+    
+    #### method 2
+    h10 = {'rocky':1, 'asap':2, 'check':3}
+    print(h10)
+    
+    
+    ### method 3
+    name = ["jack", "kitty", "nano"]
+    age = [26, 60, 50]
+    
+    h5 = {k:v for k,v in zip(name, age)}
+    print(h5)
+    
+    
+    
+    
