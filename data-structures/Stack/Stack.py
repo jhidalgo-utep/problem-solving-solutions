0949ddae83f42c877e5e6cccac72a722f1001e33
jhidalgo-utep <jhidalgo.utep@gmail.com>
2021-08-03 19:20:06 -0600

    added custom class and methods

---
+++ data-structures/Stack/Stack.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  3 18:56:57 2021
+
+@author: joaqu
+"""
+from Node import Node
+
+class Stack(object):
+    def __init__(self):
+        self.head = None
+    
+    ### Practice!!! ###
+    def insert(self, new_item):
+        if self.head == None:
+            self.head = Node(new_item)
+        else:
+            old_head = self.head
+            self.head = Node(new_item, old_head)
+    
+    def pop(self):
+        if self.head == None:
+            return
+        pop_item = self.head
+        self.head = self.head.get_next()
+        return pop_item.get_item()
+    
+    def display(self):
+        if self.head == None:
+            return
+        iter_node = self.head
+        while iter_node != None:
+            print(iter_node.get_item() )
+            iter_node = iter_node.get_next()
+        print()
--- data-structures/Stack/main.py
+++ data-structures/Stack/main.py
@@ -4,4 +4,35 @@ Created on Tue Jul 27 16:24:00 2021
 
 @author: joaqu
 """
+from Stack import Stack
 
+if __name__ == "__main__":
+    
+    print('start program\n')
+    
+    # Custom stack
+    s = Stack()
+    
+    s.insert(4)
+    s.insert(7)
+    s.insert(9)
+    s.insert(13)
+    s.insert(17)
+    s.display()
+    s.pop()
+    s.display()
+    s.insert(33)
+    s.display()
+    
+    
+    # use list as stack
+    # q = []
+    # q.append(3)
+    # q.append(7)
+    # q.append(11)
+    # q.append(9)
+    # q.pop()
+    # print(q)
+    # for i in range(len(q)-1, -1, -1 ):
+    #     print(q[i])
+    
