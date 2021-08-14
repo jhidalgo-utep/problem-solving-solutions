cde16c0b53f10bad9f9e733d2cb20873a54b5305
jhidalgo-utep <jhidalgo.utep@gmail.com>
2021-08-13 18:37:49 -0600

    finalized all hashmap methods

--- data-structures/HashMap/main.py
+++ data-structures/HashMap/main.py
@@ -6,6 +6,9 @@ Created on Mon Aug  2 19:00:42 2021
 """
 from HashMap import HashMap
 
+# 12 min old
+# 13 min new
+
 if __name__ == "__main__":
     print('start program\n')
     
@@ -18,45 +21,42 @@ if __name__ == "__main__":
     # h.display()
     
     
+    
     ### method 1
-    hm = dict()
+    h2 = dict()
+    h2['soccer'] = 11
+    h2['bball'] = 5
+    h2['hockey'] = 7
+    # h2.pop('soccer')
+    # del h2['soccer']
+    h2.update({'baseball': 12})
+    print(h2)
     
-    #insert
-    hm.update({'soccer':3})
-    hm['basketball'] = 7
-    hm['kitty'] = 10
-    hm['hockey'] = 2
-    hm.update({'polo': 8})
-    
-    #remove
-    hm.pop('soccer')
-    del hm['hockey']
-    
-    #increase value
-    hm['kitty'] = hm.get('kitty') + 1
-    hm['kitty'] += 1
-    
-    # for k in hm.keys():
-    #     print(k)
+    for h2_key in h2.keys():
+        print(h2_key)
         
-    # for v in hm.values():
-    #     print(v)
+    for h2_value in h2.values():
+        print(h2_value)
         
-    # for k,v in hm.items():
-    #     print(v)
     
     
     #### method 2
+    names = ['drake', 'jcole', 'asap', 'kendrick', '50 cent']
+    ages = [32, 33, 29, 27, 49]
+    
+    
+    h3 = {k:v for k,v in zip(names, ages)}
+    for i in h3.items():
+        print(i)
+    print('\n', h3)
+
+    
+    ### method 3
     h10 = {'rocky':1, 'asap':2, 'check':3}
     print(h10)
     
     
-    ### method 3
-    name = ["jack", "kitty", "nano"]
-    age = [26, 60, 50]
-    
-    h5 = {k:v for k,v in zip(name, age)}
-    print(h5)
+
     
     
     
